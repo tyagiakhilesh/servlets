cffe63ca6d9ff0c376a09e0c97fb6420e8fdf631
Akhilesh, Tyagi <tyagiakhilesh@gmail.com>
2018-11-06 13:04:04 +0530

    Add another servlet. Add jetty maven plugin so that I can launch the container while I work.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Run the app using shared idea run configuration.
 
 Access the servlet at `<server:port>/app/shop` path
+
+Or simply run
+`mvn jetty:run` and you can access your servlets.
---
+++ src/main/java/com/akhilesh/learning/servlet/WelcomeServlet.java
@@ -0,0 +1,37 @@
+package com.akhilesh.learning.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        this.processRequest(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        this.processRequest(req, resp);
+    }
+
+    private void processRequest(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = resp.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet WelcomeServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet WelcomeServlet at " + req.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
